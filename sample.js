bb20b04d193d77e7e0c6714348c3e31bd09a5c3f
matsunami-lvgs <kaito.matsunami@leverages.jp>
2022-09-27 15:57:15 +0900

    なんもわからん

--- sample.js
+++ sample.js
@@ -1,21 +1,33 @@
+//const file = document.getElementById('bu_acf_file');
+//input.addEventListener('change', fileInput);
+const body = new FormData();
+
+const file = document.getElementById('bu_acf_file');
+file.addEventListener('change', (e) => {
+  const hoge = e.target.files[0]
+  body.append('userfile', hoge);
+  body.append('action', 'bu_acf_article_update');
+  body.append('MAX_FILE_SIZE', '300000');
+  console.log(body)
+})
+
 function post (url){
   let request =  new XMLHttpRequest;
-  console.log('ぽよ');
+  console.log('JS:ぽよ');
   request.open ('POST', url, true);
-  request.setRequestHeader("Content-Type", "multipart/form-data");
   request.onreadystatechange =()=> {
     if (request.readyState < 4){
-      console.log('送信中');
+      console.log('JS:送信中');
     }
     if(request.readyState == 4 && request.status == 200) {
-      console.log(request.responseText);
-      console.log('完了しました');
+      console.log('JS:' + request.responseText);
+      console.log('JS:完了しました');
     }
     if(request.readyState == 4 && request.status !== 200) {
-      console.log(request.responseText);
-      console.log('ERROR');
+      console.log('JS:' + request.responseText);
+      console.log('JS:ERROR');
     }
   }
-  console.log(request.getAllResponseHeaders());
-  request.send();
+  console.log(body)
+  request.send(body);
 }
